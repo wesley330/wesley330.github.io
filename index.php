5f41cc47665241689c9f635d6f1918060d3f6b3d
Wesley <wesleytsoi1994@gmail.com>
2020-01-23 11:38:34 -0800

    Revert

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello World!
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$file = fopen("A SHIFT VERSATILITY.csv","r");
+while(! feof($file))
+  {
+  print_r(fgetcsv($file));
+  }
+fclose($file);
+?>
+
+</body>
+</html>
